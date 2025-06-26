4378d4148743e5caed004e360a3a9cfdec011ea7
Curtfoglaw <curtis@example.com>
2025-06-26 11:28:17 +1000

    adding first changes to test repo

---
+++ src/main/java/com/example/websocket_demo/WebSocketConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.websocket_demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void WebSocketMessageBrokerConfigurer(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+}
--- src/main/java/com/example/websocket_demo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocket_demo/WebsocketDemoApplication.java
@@ -3,7 +3,7 @@ package com.example.websocket_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	//Marks this class as the main entry fro the springboot application
 public class WebsocketDemoApplication {
 
 	public static void main(String[] args) {
